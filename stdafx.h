c69d6d374cdfa9005e20f034aca7909510da1abb
Geralt <parvezl123@gmail.com>
2017-04-13 15:58:24 +0200

    konfiguracja visuala

---
+++ stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifdef _WIN32
+    #include "targetver.h"
+#endif
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+#include "point.h"
+#include "rectangle.h"
+
+
+//Adding required boost header
+#include <boost/test/unit_test.hpp>
