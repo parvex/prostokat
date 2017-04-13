79ff52294ccc5eaabb93c693e2f46068e7ab0bec
Geralt <parvezl123@gmail.com>
2017-04-14 01:43:19 +0200

    przerobilem prostokat na template i zaczalem pisac klase kontenera prostokatow

---
+++ container.h
@@ -0,0 +1,56 @@
+#ifndef CONTAINER_H
+#define CONTAINER_H
+
+template<class type = int>
+struct Node
+{
+	Rect<type>* rect;
+	Node* next;
+
+};
+
+
+template <class type = int>
+class Container
+{
+private:
+	int limit;  //pojemnosc bufora
+	int actual;   //aktualna ilosc obiektow
+	//tu bedzie wektor xd
+
+public:
+	Container(int limit = 4) : limit(4) //konstruktor
+
+
+
+
+
+
+
+
+
+};
+//*************************IMPLEMENTATION
+
+template <class type>
+Container<type>::container(int limit) 
+{
+
+	
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+#endif
